0cb2fa0de9cdf190ae87f80545363f7d67c76f51
FMinister <johannes.leyrer+git@googlemail.com>
2023-10-13 02:02:17 +0200

    added solution and tests

--- median/median.go
+++ median/median.go
@@ -1,5 +1,30 @@
 package median
 
+import (
+	"sort"
+)
+
 func SingleMedian(input []int) float64 {
-	// ...
+	lengthOddOrEven := len(input) % 2
+
+	sort.Ints(input)
+
+	switch lengthOddOrEven {
+	case 0:
+		return float64(input[len(input)/2-1]+input[len(input)/2]) / 2
+	case 1:
+		return float64(input[int(len(input)/2)])
+	default:
+		return 0.0
+	}
+}
+
+func MultiMedian(input []int) []float64 {
+	var result []float64
+
+	for i := range input {
+		result = append(result, SingleMedian(input[:i+1]))
+	}
+
+	return result
 }
--- median/median_test.go
+++ median/median_test.go
@@ -22,7 +22,30 @@ func TestSingleMedian(t *testing.T) {
 
 	for _, test := range tests {
 		t.Run(test.description, func(t *testing.T) {
-			assert.Equal(t, test.expected, SingleMedian(test.input))
+			result := SingleMedian(test.input)
+			assert.Equal(t, test.expected, result)
+		})
+	}
+}
+
+func TestMultiMedian(t *testing.T) {
+	tests := []struct {
+		description string
+		input       []int
+		expected    []float64
+	}{
+		{"single item in list", []int{1}, []float64{1}},
+		{"two items in list", []int{1, 2}, []float64{1, 1.5}},
+		{"three items in list", []int{1, 2, 3}, []float64{1, 1.5, 2}},
+		{"nine items in list", []int{1, 2, 3, 4, 5, 6, 7, 8, 9}, []float64{1, 1.5, 2, 2.5, 3, 3.5, 4, 4.5, 5}},
+		{"ten items in list", []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}, []float64{1, 1.5, 2, 2.5, 3, 3.5, 4, 4.5, 5, 5.5}},
+		{"example list", []int{17, 2, 8, 27, 12, 9}, []float64{17, 9.5, 8, 12.5, 12, 10.5}},
+	}
+
+	for _, test := range tests {
+		t.Run(test.description, func(t *testing.T) {
+			result := MultiMedian(test.input)
+			assert.Equal(t, test.expected, result)
 		})
 	}
 }

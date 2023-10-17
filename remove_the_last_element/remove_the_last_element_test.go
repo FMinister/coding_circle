d5bdfd4e365d448778907e57fe4ceea6de0ef4e0
FMinister <johannes.leyrer+git@googlemail.com>
2023-10-17 21:16:22 +0200

    added solution and tests

---
+++ remove_the_last_element/remove_the_last_element_test.go
@@ -0,0 +1,41 @@
+package removethelastelement_test
+
+import (
+	"testing"
+
+	rtle "github.com/FMinister/coding_circle/remove_the_last_element"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestRemoveTheLastElement(t *testing.T) {
+	t.Run("Remove the last element", func(t *testing.T) {
+		list := rtle.NewNode(1, rtle.NewNode(3, rtle.NewNode(5, rtle.NewNode(7, rtle.NewNode(9, nil)))))
+		updatedList := rtle.RemoveTheLastElement(list, 1)
+		values := rtle.GetListValues(updatedList)
+
+		assert.Equal(t, []int{1, 3, 5, 7}, values)
+	})
+
+	t.Run("Remove the first element", func(t *testing.T) {
+		list := rtle.NewNode(1, rtle.NewNode(3, rtle.NewNode(5, rtle.NewNode(7, rtle.NewNode(9, nil)))))
+		updatedList := rtle.RemoveTheLastElement(list, 5)
+		values := rtle.GetListValues(updatedList)
+
+		assert.Equal(t, []int{3, 5, 7, 9}, values)
+	})
+
+	t.Run("Remove the i-th element", func(t *testing.T) {
+		list := rtle.NewNode(1, rtle.NewNode(3, rtle.NewNode(5, rtle.NewNode(7, rtle.NewNode(9, nil)))))
+		updatedList := rtle.RemoveTheLastElement(list, 3)
+		values := rtle.GetListValues(updatedList)
+
+		assert.Equal(t, []int{1, 3, 7, 9}, values)
+	})
+
+	t.Run("Remove nothing because i is too large", func(t *testing.T) {
+		list := rtle.NewNode(1, rtle.NewNode(3, rtle.NewNode(5, rtle.NewNode(7, rtle.NewNode(9, nil)))))
+		updatedList := rtle.RemoveTheLastElement(list, 6)
+
+		assert.Nil(t, updatedList)
+	})
+}

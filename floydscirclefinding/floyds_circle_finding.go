ad0e02dab02e278799109443f365c7ee35ac625a
FMinister <johannes.leyrer+git@googlemail.com>
2023-11-17 16:42:33 +0100

    add solution

--- floydscirclefinding/floyds_circle_finding.go
+++ floydscirclefinding/floyds_circle_finding.go
@@ -1,5 +1,17 @@
 package floydscirclefinding
 
 func FindCircle(head *Node) bool {
+	slow := head
+	fast := head
+
+	for slow != nil && fast != nil && fast.Next != nil {
+		slow = slow.Next
+		fast = fast.Next.Next
+
+		if slow == fast {
+			return true
+		}
+	}
+
 	return false
 }

224cf1c7319317284b51839cd7ead036b5dadb34
FMinister <johannes.leyrer+git@googlemail.com>
2023-11-17 16:45:56 +0100

    add some more tests

--- floydscirclefinding/floyds_circle_finding_test.go
+++ floydscirclefinding/floyds_circle_finding_test.go
@@ -14,12 +14,31 @@ func TestFindCircle(t *testing.T) {
 		expected    bool
 	}{
 		{
-			description: "No circle",
+			description: "No circle, single node",
+			list:        fcf.NewNode(1, nil),
+			expected:    false,
+		},
+		{
+			description: "No circle, uneven number of nodes",
 			list:        fcf.NewNode(1, fcf.NewNode(3, fcf.NewNode(5, fcf.NewNode(7, fcf.NewNode(9, nil))))),
 			expected:    false,
 		},
 		{
-			description: "Circle",
+			description: "No circle, even number of nodes",
+			list:        fcf.NewNode(1, fcf.NewNode(3, fcf.NewNode(5, fcf.NewNode(7, fcf.NewNode(9, fcf.NewNode(11, nil)))))),
+			expected:    false,
+		},
+		{
+			description: "Circle, uneven number of nodes",
+			list: func() *fcf.Node {
+				node := fcf.NewNode(12, fcf.NewNode(8, fcf.NewNode(17, fcf.NewNode(2, fcf.NewNode(27, fcf.NewNode(9, fcf.NewNode(33, nil)))))))
+				node.Next.Next.Next.Next.Next.Next.Next = node.Next
+				return node
+			}(),
+			expected: true,
+		},
+		{
+			description: "Circle, even number of nodes",
 			list: func() *fcf.Node {
 				node := fcf.NewNode(12, fcf.NewNode(8, fcf.NewNode(17, fcf.NewNode(2, fcf.NewNode(27, fcf.NewNode(9, nil))))))
 				node.Next.Next.Next.Next.Next.Next = node.Next.Next.Next

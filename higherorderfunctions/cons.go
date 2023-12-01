bc87743888512e295c574309e70b387243671728
FMinister <johannes.leyrer+git@googlemail.com>
2023-12-01 22:26:00 +0100

    fix typo

--- higherorderfunctions/cons.go
+++ higherorderfunctions/cons.go
@@ -2,7 +2,7 @@ package higherorderfunctions
 
 type PairFunc func(int, int) int
 
-func COns(a, b int) func(PairFunc) int {
+func Cons(a, b int) func(PairFunc) int {
 	return func(f PairFunc) int {
 		return f(a, b)
 	}
